3a0eae68fa5d51133b054674a4e337b48e32af21
patress99 <49107052+patress99@users.noreply.github.com>
2021-10-19 20:38:48 +0200

    ic

---
+++ src/aus2/sp1/AUS2SP1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aus2.sp1;
+
+/**
+ *
+ * @author Pato
+ */
+public class AUS2SP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       TTTree<Integer> tree = new TTTree<Integer>();
+       int a = 10;
+       int b = 20;
+       
+       
+       //insert data, key
+      tree.insert(10);
+      tree.delete(10);
+      //tree.insert(20);
+    //  tree.insert(30);
+      
+  //    //todo tu to pada
+   // tree.insert(40);
+   // tree.insert(15);     
+   // tree.insert(25);
+   // tree.insert(6);
+   // tree.insert(3);
+   // tree.insert(4);
+    //tree.insert(26);
+    //tree.insert(27);
+    
+    }
+    
+}
